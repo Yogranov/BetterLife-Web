f8848b42a97b02a36bdcbb64ab4c5b7953e9e076
Yogi <frizen700@gmail.com>
2020-01-13 20:08:03 +0200

    - Change: index now is the main template - Change: add style to main css - Add: new icons and favicon - Add: fontAwsome icons

--- public/index.php
+++ public/index.php
@@ -6,30 +6,132 @@ error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 ?>
 
-<html lang="he">
+<html lang="he" xmlns="http://www.w3.org/1999/html">
 <head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="system/bootstrap/css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+    <link rel="stylesheet" href="system/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="system/aos/aos.css">
+    <link rel="stylesheet" href="system/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="core/css/main.css">
+
+    <title>BetterLife</title>
+    <link rel="icon" href="media/favicon.png">
 
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-lg-5 shadow-sm mb-5 flex-row-reverse">
+    <a class="navbar-brand pl-lg-3" href="#">
+        <img src="media/logos/BetterLifeLogo.png" alt="logo">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">ראשי</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">כתבות</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">אודות</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">צור קשר</a>
+            </li>
+        </ul>
+
+        <a class="nav-item mr-auto ml-lg-2" href="#" style="color: rgba(0,0,0,0.5)">
+            <i class="fas fa-sign-in-alt"></i>
+            התחבר
+        </a>
+
+    </div>
+</nav>
 <div class="container">
-    <h1 data-aos="fade-up">Test</h1>
+<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
+
+<p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet, consectetur, adipiscin] velit, sed quia non numquam do eius modi tempora incididunt, ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+<p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur.</p>
+
+<h2>Vero eos et accusamus et iusto odio dignissimos ducimus</h2>
+
+<p>Qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. </p>
+
+<p>Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Qua temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae pondere ad lineam. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat</p>
+
+<p>Quibus ego assentior, dum modo de iisdem rebus ne Graecos quidem legendos putent. Res vero bonaa verbis electis graviter omateque dictas quis i legat? Nisi qui se plane Graeciun dici velit, ut a 9 Scaeiola est praetore salutatus Athenis Albucius. Quem quidem locum cum multa venustate et omm sale idem Lucilius, apud quem praeclare Scaevola.</p>
+
+<p>Qui autem alia matunt scribi a nobis, aequi esse debent, quod et seripta multa sunt, sic ut plura nemini e nostris, et scribentur fortasse plura si vita suppetet; et tamen qui diligenter haec quae de philosophia Htteris mandamus legere assueverit, iudicabit nulla ad legendum his esse potiora.</p>
+
+<h3>Tempore intellegi convenire</h3>
+
+<p>Epicurus autem, in quibus sequitur Democritum, noil fere labitur, Quam- quam utriusque cum mutta non prolx). turn illiid in priniis, quoJ, cum in rerum nalura duo quaerenda sint, ununi quae materia sit ex qua quaeque res cfficiatur, alterum quae vis sit quae quidque efficiat, de materia disserucrunt, vim et causam efficiendi reliquerunt. Sed lioc commune vitiuni; illae Epicur propriae ruinae: censet enim eadem ilia indlvidua e solida corpora ferri deorsum suo pondere ad lineam i hunc naturalem esse omnium corporum motuni.</p>
+
+<p>Deinde ibidem homo acutus, cam illud occorreret, j omnia deorsum e regione ferrentur et, ut dixi, ad lineam, numquam fore ut atomus altera alteram posset attingere, itaque attulit rem commenticiam.</p>
+
+<p>Declinare dixit atomum perpaulum, quo nihil posset fieri minus; ita eifici complexiones et copulationes et adhaesiones atomorum inter se, ex quo eificeretur mundus omnesque partes mundi quaeque in eo essent. Quae cum res tota fieta sit piieriliter, turn ne efficit quidem^ quod vult. Nam et ipsa declinatio ad libidinem fiiigitur - ait enim deelinare atomum sine causa, quo nibil turpius physico quam fieri.</p>
+
+
 </div>
 
-<script src="system/bootstrap/js/bootstrap.js"></script>
+
+
+<script src="system/jquary/jquery-3.4.1.min.js"></script>
+<script src="system/bootstrap/js/bootstrap.min.js"></script>
 <script src="system/aos/aos.js"></script>
 <script>
     AOS.init();
 </script>
 </body>
+
+<footer class="page-footer font-small blue-grey lighten-5 mt-5 border-top">
+    <div class="container text-center pt-1 mt-3">
+        <div class="row mt-3 dark-grey-text flex-row-reverse">
+
+            <div class="col-4 text-left">
+                <h6 class="text-uppercase font-weight-bold">צור קשר</h6>
+                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>050-333-0412<i class="fas fa-home mr-3"></i> </p>
+                <p>frizen700@gmail.com<i class="fas fa-envelope mr-3"></i></p>
+                <p>Kfar - Warburg, Hadarom<i class="fas fa-phone mr-3"></i></p>
+            </div>
+
+            <div class="col-4">
+                <div class="row mt-5">
+                    <div class="col-4">
+                        <a href="#" class="fab fa-facebook fa-3x" style="color:#45619D"></a>
+                    </div>
+                    <div class="col-4">
+                        <a href="#" class="fas fa-envelope-open-text fa-3x" style="color:#D44C3D"></a>
+                    </div>
+                    <div class="col-4">
+                        <a href="#" class="fab fa-twitter-square fa-3x" style="color:#7CBBEB"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 text-right">
+                <h6 class="font-weight-bold">חזור למעלה</h6>
+                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 100px;">
+                <p><i class="fas fa-home ml-3"></i>לכל הכתבות</p>
+                <p><i class="fas fa-envelope ml-3"></i>צוות הרופאים</p>
+                <p><i class="fas fa-phone ml-3"></i>אודות</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="footer-copyright text-center text-black-50 py-3">© BetterLife 2020</div>
+</footer>
+
+
+
 </html>
 
 
